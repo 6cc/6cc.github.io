f20de33ec356279b262989540ca375953a32e4f8
6cc <34774452+6cc@users.noreply.github.com>
2025-01-05 11:59:06 +0800

    m5izmqsz

--- source/quartz/plugins/transformers/jsts.ts
+++ source/quartz/plugins/transformers/jsts.ts
@@ -3,17 +3,21 @@ import { QuartzTransformerPlugin } from "../types"
 export const jsts: QuartzTransformerPlugin = () => {
   return {
     name: "jsts",
-    css: [
-      // base css
-      "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/atex.min.css",
-    ],
-    js: [
-      {
-        // fix copy behaviour: https://github.com/KaTeX/KaTeX/blob/main/contrib/copy-tex/README.md
-        src: "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/contrib/opy-tex.min.js",
-        loadTime: "afterDOMReady",
-        contentType: "external",
-      },
-    ],
+    externalResources() {
+      return {
+        css: [
+          // base css
+          "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/atex.min.css",
+        ],
+        js: [
+          {
+            // fix copy behaviour: https://github.com/KaTeX/KaTeX/blob/main/contrib/copy-tex/README.md
+            src: "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/contrib/opy-tex.min.js",
+            loadTime: "afterDOMReady",
+            contentType: "external",
+          },
+        ],
+      }
+    },
   }
 }
