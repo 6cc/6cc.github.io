93cd32b5cc3f5757fb2adc9d352fc75f12ead3e6
6cc <34774452+6cc@users.noreply.github.com>
2026-03-22 20:52:55 +0800

    loc_date-getDate-SingleD-DoubleD

--- 9/0_greasyFork-2.js
+++ 9/0_greasyFork-2.js
@@ -1,43 +1,21 @@
-const menuActions = {
-  "像素级复刻": () => {
-    // 创建 jsPanel
-    // 注意：如果是官方 ESM 版，jsPanel 可能是对象里的一个属性
-    const panel = (jsPanel.jsPanel || jsPanel).create({
-      headerTitle: '系统底座',
-      content: '资源加载成功！'
-    });
-  },
-  "丝滑动效": () => console.log("执行功能 B"),
-  "功能f": () => { document.body.style.background = "#2b3e50"; },
-  "default": (name) => console.log(`点击了: ${name}，但未定义具体函数。`),
+// https://github.com/wy580477/PaaS-Related/blob/main/CF_Workers_Reverse_Proxy.md
+const SingleDay = 'cloud1.herokuapp.com';
+const DoubleDay = 'cloud2.herokuapp.com';
+const timezone = 'Etc/GMT+2';
+
+export default {
+  async fetch(request, env, ctx) {
+    let localized_date = new Date(
+      new Date().toLocaleString('en-US', { timeZone: timezone })
+    );
+    
+    let host = localized_date.getDate() % 2 ? SingleDay : DoubleDay;
+    
+    let url = new URL(request.url);
+    url.hostname = host;
+    
+    let newRequest = new Request(url, request);
+    
+    return fetch(newRequest);
+  }
 };
-
-(async () => {
-    // 1. 导入加载器
-    const { referLibrary } = await import('https://gcore.jsdelivr.net/gh/6cc/6cc.github.io/9/1_referLibrary.js');
-
-    // 2. 资源加载：使用具有语义的 Key
-    await referLibrary({
-        'menuCss': 'https://gcore.jsdelivr.net/gh/6cc/6cc.github.io/9/menuAT.css',     // ID将是 style-menuStyle
-        'menuAT':    'https://gcore.jsdelivr.net/gh/6cc/6cc.github.io/9/2_menu-AT-3.js',
-        'panelCss':'https://gcore.jsdelivr.net/gh/Flyer53/jsPanel4/es6module/jspanel.min.css',
-        'jsPanel':   'https://gcore.jsdelivr.net/gh/Flyer53/jsPanel4/es6module/jspanel.min.js',
-    });
-
-    // 3. 直接调用！ 
-    // 因为新版 referLibrary 已经帮你把这些模块挂载到了 window.menuAT 和 window.jsPanel
-    
-    // 生成容器 (假设 implantContainer 已经在 menuAT 中 export)
-    menuAT.implantContainer(); 
-    
-    // 渲染菜单
-    menuAT.renderMenu(`
-- 核心引擎
-  - 渲染器
-    - 像素级复刻
-    - 丝滑动效
-  - 物理系统
-- 资源管理
-- 功能f
-    `, 'menu-container', menuActions);
-})();
